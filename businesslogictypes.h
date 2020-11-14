37ec8838fd1f019ff0891ae467e5442a83db91a6
VEGA <VEGA@localhost.localdomain>
2020-11-14 15:59:25 +0100

    add Operations struct

--- businesslogictypes.h
+++ businesslogictypes.h
@@ -5,11 +5,18 @@
  *	businessLogicTypes.h : types représentant la    *
  *  logique métier                                  */
 #ifndef __BUSINESS_LOGIC_TYPES__
+
+typedef struct
+{
+    signed int amount;
+    char* date;
+} operations;
+
 typedef struct
 {
    char* accountId;
    signed int balance;
-
+   operations* ops;
 }  account;
 // Un client à un Id, un password et des ids de compte
 typedef struct
